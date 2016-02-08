b791c96476204ac39f827ffed78cfe7e501d3fad
AndrewEdwards <edwards.ac@gmail.com>
2016-02-08 16:06:15 +0900

    Add support for sdl based projects

--- src/app.d
+++ src/app.d
@@ -1,4 +1,3 @@
-import std.stdio;
 import std.algorithm;
 import std.file;
 import std.datetime;
@@ -7,37 +6,57 @@ import core.thread;
 import std.json;
 import std.conv;
 
+import sdlang;
+import io.text;
+
 void main()
 {
+  string content, projectName;
+  
+  if (exists("dub.json"))
+  {
+    content = to!string(read("dub.json"));
+    JSONValue doc = parseJSON(content).object;
+    projectName = doc.object["name"].str;
+  }
+  else if (exists("dub.sdl"))
+  {
+    content = to!string(read("dub.sdl"));
+    Tag root = parseSource(content);
+    projectName = to!string(root.tags["name"][0].values[0]);
+  }
 
-auto content = to!string(read("dub.json"));
-JSONValue doc = parseJSON(content).object;
-string projectName = doc.object["name"].str;
-writeln("found dub project: ",projectName);
+  println("found dub project: ",projectName);
 
   bool reload = false;
-    while(true){
+  while(true)
+  {
 
-try{
+    try
+    {
+      foreach(file ; dirEntries(".","*.{d,dt}",SpanMode.depth))
+      {
+        if(file.timeLastModified > Clock.currTime() - 3.seconds){
+          reload = true;
+          break;
+        }
 
-  foreach(file ; dirEntries(".","*.{d,dt}",SpanMode.depth)){
-       if(file.timeLastModified > Clock.currTime() - 3.seconds){
-         reload = true;
-         break;
-       }
+        if (reload)
+        {
+          println("reloading");
 
-       if(reload){
-         writeln("reloading");
+          spawnShell("killall " ~ projectName ~ " ; dub build && ./" ~ projectName ~ " &");
 
-spawnShell("killall " ~ projectName ~ " ; dub build && ./" ~ projectName ~ " &");
+          Thread.sleep( dur!("seconds")( 7 ) );
 
-Thread.sleep( dur!("seconds")( 7 ) );
-
-         reload = false;
-         writeln("done reloading");
-       }
-  }} catch(Exception e){
-        writeln("oops");
-       }
- }
+          reload = false;
+          println("done reloading");
+        }
+      }
+    }
+    catch(Exception e)
+    {
+      println("oops");
+    }
+  }
 }
